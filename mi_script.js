d04b8f3e1f9fa948a431baaeb5e985e5ea2e7e0d
Basurto589 <a100252589@my.uvm.edu.mx>
2025-04-30 13:24:13 -0600

    mi_script.js

---
+++ mi_script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const boton = document.getElementById("interactivo");
+    boton.addEventListener("click", function () {
+        alert("¡Bienvenido(a)! Gracias por visitar nuestro proyecto sobre Igualdad de Género.");
+    });
+});
